2c584e0dfd1dc8ca73b5ac7596b9e91deb5d82a5
TheFiveMCoders <44032346+thefivemcoders@users.noreply.github.com>
2021-06-15 17:06:28 +0530

    Create en.lua

---
+++ locales/en.lua
@@ -0,0 +1,11 @@
+Locales['en'] = {
+	['shop'] = 'FastFood Drivethrough',
+	['shops'] = 'FastFood Drivethrough',
+	['press_menu'] = 'Press ~INPUT_CONTEXT~ Make order .',
+	['bought'] = 'you bought ~y~%sx~s~ ~b~%s~s~ for ~r~$%s~s~',
+	['not_enough'] = 'You Dont have sufficient ~r~fund~s~, you are  ~r~$%s~s~ Short!',
+	['player_cannot_hold'] = 'You Dont have ~r~Enough~s~ ~y~space~s~ in your inventory!',
+	['shop_confirm'] = 'confirm %sx %s for €%s?',
+	['no'] = 'No',
+	['yes'] = 'Yes',
+}
